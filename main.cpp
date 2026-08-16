6632edcb02f3028c3f511fbf05ce81f2b905ad1f
Atomizr <pimenovvlad354@gmail.com>
2026-08-16 22:20:32 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,5 +6,7 @@ int main(int argc, char *argv[])
     QApplication app(argc, argv);
     BrowserWindow window;
     window.show();
+    window.raise();
+    window.activateWindow();
     return app.exec();
 }
