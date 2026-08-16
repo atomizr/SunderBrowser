7f5f13dd0457797cb67c59d5276582ef193b8f73
Atomizr <pimenovvlad354@gmail.com>
2026-08-16 23:25:37 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,9 +4,19 @@
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
+
+    // Метаданные приложения
+    app.setOrganizationName("Atomizr");
+    app.setApplicationName("SunderBrowser");
+    app.setApplicationVersion("1.0.0");
+
+    // Устанавливаем иконку окна из ресурсов
+    app.setWindowIcon(QIcon(":/app.ico"));
+
     BrowserWindow window;
     window.show();
     window.raise();
     window.activateWindow();
+
     return app.exec();
 }
